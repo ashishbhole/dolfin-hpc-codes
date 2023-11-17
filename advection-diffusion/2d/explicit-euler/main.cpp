cd2a46de58d9cf1b2359509d703a378d5a8812f2
Ashish Bhole <ashishbhole07@gmail.com>
2023-11-17 14:52:30 +0100

    rearranged ufl and cpp files, added comments, works with single process, multiple process runs needs attention

--- advection-diffusion/2d/explicit-euler/main.cpp
+++ advection-diffusion/2d/explicit-euler/main.cpp
@@ -2,7 +2,16 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // Modified by Niclas Jansson 2017.
-// Modified by Ashish Bhole 2023: Modified from ConvectionDiffusion demo
+// Modified by Ashish Bhole 2023: 
+// Modified from ConvectionDiffusion demo
+//
+// This is an IVP tp solve linear advection equation.
+// The initial condition is specified as a Gaussian function.
+// It advects with constant velocity vector = {adv_x, adv_y}.
+//
+//     u_t + \mathbb{c} \cdot \nabla u = 0
+//     u(x, 0) = exp(-alpha*( (x-x0)**2 + (y-y0)**2 ))
+//     u_ex(x ,t) = exp(-alpha*( (x-adv_x*t-x0)**2 + (y-adv_y*t-y0)**2 ))
 
 #define IO
 
@@ -17,6 +26,7 @@ real speed_x = 0.2;
 real speed_y = 0.2;
 real Tfinal  = 1.0;
 
+// Analytic function to specify the initial condition and exact solution.
 struct ExactSolution : public Value< ExactSolution, 1 >
 {
   // Constructor
@@ -31,6 +41,7 @@ struct ExactSolution : public Value< ExactSolution, 1 >
   double t, alpha, speed_x, speed_y;
 };
 
+// can be simplified for 0.0 boundary condition. kept for generality.
 struct DirichletFunction : public Value<DirichletFunction, 1>
 {
   void eval(real *value, const real *x) const
@@ -39,6 +50,7 @@ struct DirichletFunction : public Value<DirichletFunction, 1>
   }
 };
 
+// identify boundaries
 struct DirichletBoundary : public SubDomain
 {
   bool inside(const real* x, bool on_boundary) const
@@ -47,12 +59,15 @@ struct DirichletBoundary : public SubDomain
   }
 };
 
+// File to write the numerical solution
 File file("solution.pvd");
 
 int main(int argc, char **argv)
-{
+{ 
+  // specify number of time the mesh shoould be refined	
   uint num_refine = 0;
 
+  // take num_refine from command line argument
   if (argc > 1)
   {
       for (uint i = 0; argv[1][i] != '\0'; i++)
@@ -73,9 +88,6 @@ int main(int argc, char **argv)
     
   dolfin_init(argc, argv);
 
-  // Create mesh
-  //Mesh mesh("mesh/UnitSquareMesh_32x32.bin");
-
   uint Np = pow(2,num_refine)*32;
   UnitSquare mesh(Np, Np);
 
@@ -84,8 +96,8 @@ int main(int argc, char **argv)
   DirichletBoundary boundary;
   DirichletBC bc(u0, mesh, boundary);
 
+  // For initial conditions 
   double t = 0.0;
-  // Set solution to initial condition
   ExactSolution Gaussian;
   Gaussian.alpha = 128.0;
   Gaussian.speed_x = speed_x;
@@ -93,55 +105,64 @@ int main(int argc, char **argv)
   Gaussian.t = t;
   Analytic<ExactSolution> ui( mesh, Gaussian);
 
+  // time step computation
   double Nc = 0.05;
-  double h = MeshQuality(mesh).h_max; //1.0/sqrt(N*N);
+  double h = MeshQuality(mesh).h_max;
   tstep = Nc * h / (sqrt(pow(speed_x,2)+pow(speed_y,2)));
 
+  // some quantities in form files
   Constant dt(tstep);
   Constant adv_x(speed_x);
   Constant adv_y(speed_y);
   Constant tau(0.0*h/(sqrt(pow(speed_x,2)+pow(speed_y,2))));
 
-  AdvectionDiffusion::BilinearForm a(mesh);
-  Function u(a.trial_space());
+  // See the declaration in the header file
+  AdvectionDiffusion::BilinearForm a(mesh, adv_x, adv_y, tau, dt);
   Function u1(a.trial_space());
+  Function un(a.trial_space());
 
-  //Interpolate from an expression or a coefficient or a generic function 
-  u << ui;
+  // interpolate the initial condition to initialize the problem
+  un << ui;
 
-  // Beware: arguments must be called in a sequence matched by the call in header file.
-  // Need to confirm this fact from developers.
-  AdvectionDiffusion::LinearForm L(mesh, u, adv_x, adv_y, tau, dt);
+  AdvectionDiffusion::LinearForm L(mesh, un);
 
+  // declare and assemble FE Matrix and vector
   Matrix A;
   Vector b;
   a.assemble(A, true);
 
+  // specify linear solver
   KrylovSolver solver(bicgstab, bjacobi);
   uint step = 0;
+
+  // write the initial condition to the solution file
   #ifdef IO
-    file << u;
+  file << un;
   #endif
   
   while (t < Tfinal)
   {
     // Adjust dt to reach final time exactly
     if (t+tstep > Tfinal) dt = Tfinal - t;
-    L.assemble(b, step == 0);
+    L.assemble(b, step==0);
     bc.apply(A, b, a);
-    solver.solve(A, u.vector(), b);
-    u.sync();
-    u1 = u;
+    solver.solve(A, u1.vector(), b);
+    u1.sync();
+    un = u1;
     t +=tstep;
     step += 1;
     #ifdef IO
-    if (step%10 == 0) file << u; 
+    if (step%10 == 0) file << u1; 
     #endif
   }
+  // Get the exatc solution at Tfinal
   Gaussian.t = Tfinal;
   Analytic<ExactSolution> uex( mesh, Gaussian);
-  u1 << uex;
-  u1 -= u;
+  // interpolate the exact solution to FE function
+  Function ue(a.trial_space());
+  ue << uex;
+  // Compute the numerical error in u1 as : u1 = u1 - ue
+  u1 -= ue;
   message( "h, Error l1, l2, linf norm: %e %e %e %e", h, u1.vector().norm(l1), u1.vector().norm(l2), u1.vector().norm(linf) );
 
   dolfin_finalize();
