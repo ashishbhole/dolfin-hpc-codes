f8dcc3815006200abdf2362a5d9d99cd3ef7ae35
Ashish Bhole <ashishbhole07@gmail.com>
2023-12-13 18:21:39 +0100

    use projection for sources in wave equation, but we do not need projection actually

--- wave_equation/2d/cd2/main.cpp
+++ wave_equation/2d/cd2/main.cpp
@@ -15,15 +15,14 @@
 #include "Projection.h"
 #include <sstream>
 #include <dolfin.h>
+#include <fstream>
 
 using namespace dolfin;
 
 real tstep = 0.01;
 real speed = 1.0;
-real Tfinal = 5.0;
-
-real rec1[3] = {3.0, 3.0, 3.0};
-real u_values[1] = {0.0};
+real Tfinal = 100.0;
+real Nc = 0.01;
 
 // Analytic function to specify the initial condition and exact solution.
 struct InitialCondition : public Value< InitialCondition, 1 >
@@ -43,9 +42,9 @@ struct Source : public Value< Source, 1 >
   Source(): t(0), alpha(32.0) {}	
   void eval( real * values, const real * x ) const
   {
-    if(fabs(x[0]-0.5)<0.05 && fabs(x[1]-0.5)<0.05)
+    if( (sqrt(x[0]*x[0] + x[1]*x[1]) ) <= 3.6775 + abs(DOLFIN_EPS) )
     {	    
-      values[0] = sin(10*t); //exp(-alpha*((x[0]-0.5)*(x[0]-0.5)+(x[1]-0.5)*(x[1]-0.5))) * exp(-alpha*t*t); //
+      values[0] = sin(10.0*DOLFIN_PI*t);
     }
     else
     {
@@ -71,14 +70,32 @@ struct DirichletBoundary : public SubDomain
   }
 };
 
+void project(Function &ff, Mesh &mesh, Source &src, double t,
+             Matrix &Ap, Vector &bp, 
+	     KrylovSolver &solver, bool flag)
+{
+  src.t=t;
+  Analytic<Source> f( mesh, src);
+  Projection::LinearForm Lp(mesh, f);
+  Lp.assemble(bp, flag);
+  solver.solve(Ap, ff.vector(), bp);
+  ff.sync();
+}
+
 File file("solution.pvd");
+std::ofstream outfile ("signals.txt");
 
 int main(int argc, char **argv)
 { 
   dolfin_init(argc, argv);
 
+  real rec1[3] = { 4.0, 0.5, 0.0};
+  real rec2[3] = {-4.0, 0.5, 0.0};
+  real u_values1[1] = {0.0};
+  real u_values2[1] = {0.0};
+
   // Parallel file writing does not work with in-built meshes.
-  Mesh mesh("rectangular_struct_tria.bin");
+  Mesh mesh("disc_in_disc.bin");
 
   Analytic<DirichletFunction> u0(mesh);
   DirichletBoundary boundary;
@@ -91,7 +108,6 @@ int main(int argc, char **argv)
 
   Source src;
 
-  double Nc = 0.05;
   double h = MeshQuality(mesh).h_max;
   tstep = Nc * h / speed;
   Constant dt(tstep);
@@ -107,27 +123,36 @@ int main(int argc, char **argv)
   a.assemble(A, true);
   KrylovSolver solver(bicgstab, bjacobi);
 
+  Projection::BilinearForm ap(mesh);
+  Matrix Ap;
+  Vector bp;
+  ap.assemble(Ap, true);
+
   uint step = 0;
   while (t < Tfinal)
   {
     if (t+tstep > Tfinal) dt = Tfinal - t;
-    src.t=t;
-    Analytic<Source> f( mesh, src);
-    ff << f;
+    project(ff, mesh, src, t, Ap, bp, solver, step==0);
+
     L.assemble(b, step==0);
     bc.apply(A, b, a);
     solver.solve(A, u.vector(), b);
     u.sync();
     u_old = un;
     un = u;
-    //u.eval(u_values, rec1);
-    message("u(rec1) = %g", u.vector()[20000]);   
     #ifdef IO
-    if (step%10 == 0) file << u; 
+    if (step%100 == 0) file << u; 
     #endif
+
+    u.eval(u_values1, rec1);
+    u.eval(u_values2, rec2);
+    message("t, u1, u2 = %g %g %g", t, u_values1[0], u_values2[0]);
+    outfile << t << " " << u_values1[0] << " " << u_values2[0] << "\n";
+
     t +=tstep;
     step += 1;
   }
+  outfile.close();  
   message( "t, h, l1, l2, linf norm: %e %e %e %e", t, h, u.vector().norm(l1), u.vector().norm(l2), u.vector().norm(linf) );
   dolfin_finalize();
   return 0;
