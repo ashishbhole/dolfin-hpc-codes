e818afbc701c2dde4d25afafb7f1bdd9e112ef2c
Ashish Bhole <ashishbhole07@gmail.com>
2023-12-21 11:38:23 +0100

    adding 1d Burgers equation solver

---
+++ burger/1d/implicit_euler/main.cpp
@@ -0,0 +1,130 @@
+// Copyright (C) 2010 Jeannette Spuehler.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// Modified by Niclas Jansson 2017.
+// Modified by Ashish Bhole 2023: 
+// Modified from ConvectionDiffusion demo
+//
+// This is an IVP tp solve linear advection equation.
+// The initial condition is specified as a Gaussian function.
+// It advects with constant velocity vector = {adv_x, adv_y}.
+//
+//     u_t + \mathbb{c} \cdot \nabla u = 0
+//     u(x, 0) = exp(-alpha*( (x-x0)**2 + (y-y0)**2 ))
+//     u_ex(x ,t) = exp(-alpha*( (x-adv_x*t-x0)**2 + (y-adv_y*t-y0)**2 ))
+
+#define IO
+
+#include "Burger.h"
+#include "Projection.h"
+
+#include <sstream>
+#include <dolfin.h>
+
+using namespace dolfin;
+
+real tstep = 0.0001;
+real Tfinal  = 1.0;
+
+// Analytic function to specify the initial condition and exact solution.
+struct AnalyticFunction : public Value< AnalyticFunction, 1 >
+{
+  // Constructor
+  AnalyticFunction() : t(0), alpha(32.0) {}	
+  void eval( real * values, const real * x ) const
+  {
+    values[0] = sin(2.0*DOLFIN_PI*x[0]);
+  }
+  // Current time
+  double t, alpha;
+};
+
+// can be simplified for 0.0 boundary condition. kept for generality.
+struct DirichletFunction : public Value<DirichletFunction, 1>
+{
+  void eval(real *value, const real *x) const
+  {
+    value[0] = 0.0;
+  }
+};
+
+// identify boundaries
+struct DirichletBoundary : public SubDomain
+{
+  bool inside(const real* x, bool on_boundary) const
+  {
+    return on_boundary;
+  }
+};
+
+// File to write the numerical solution
+File file("solution.pvd");
+
+int main(int argc, char **argv)
+{ 
+  dolfin_init(argc, argv);
+
+  // Parallel file writing does not work with in-built meshes.
+  UnitInterval mesh(1000);
+
+  // Set boundary conditions
+  Analytic<DirichletFunction> ub(mesh);
+  DirichletBoundary boundary;
+  DirichletBC bc(ub, mesh, boundary);
+
+  // For initial conditions 
+  double t = 0.0;
+  AnalyticFunction Gaussian;
+  Gaussian.alpha = 128.0;
+  Analytic<AnalyticFunction> ui( mesh, Gaussian);
+
+  // Note that interpolation operation u0<<ui only works for degree=1.
+  // Hence projection of the analytic function on FE space is used as
+  // the initial condition.
+  Projection::BilinearForm a1(mesh);
+  Projection::LinearForm L1(mesh, ui);
+  Matrix A1;
+  Vector b1;
+  a1.assemble(A1, true);
+  L1.assemble(b1, true);
+  Function u0(a1.trial_space());
+  KrylovSolver solver(bicgstab, bjacobi);
+  solver.solve(A1, u0.vector(), b1);
+  u0.sync();
+  
+  // some quantities in form files
+  Constant dt(tstep);
+
+  // See the declaration in the header file
+  Burger::BilinearForm a(mesh, u0, dt);
+  Function du(a.trial_space());
+  Burger::LinearForm L(mesh, u0, dt);
+  Matrix A;
+  Vector b;
+  a.assemble(A, true);
+
+  uint step = 0;
+
+  // write the initial condition to the solution file
+  #ifdef IO
+  file << u0;
+  #endif
+  
+  while (t < Tfinal)
+  {
+    L.assemble(b, step==0);
+    bc.apply(A, b, a);
+    solver.solve(A, du.vector(), b);
+    du.sync();
+    u0 += du;
+    t +=tstep;
+    step += 1;
+    #ifdef IO
+    if (step%100 == 0) file << u0; 
+    #endif
+  }
+  message( "Solution norm l1, l2, linf: %e %e %e", u0.vector().norm(l1), u0.vector().norm(l2), u0.vector().norm(linf) );
+
+  dolfin_finalize();
+  return 0;
+}
