4e4abd81c9e3afd84e891e931854ffbff29199cb
Ashish Bhole <ashishbhole07@gmail.com>
2024-01-15 10:06:23 +0100

    updating 3d projection demo for time dependence, it does not seem to work with mutiple processes

--- projection/3d/main.cpp
+++ projection/3d/main.cpp
@@ -8,14 +8,16 @@
 
 using namespace dolfin;
 
-//------------------------------------------------------------------------------
-struct F : public Value<F>
+struct Fun : public Value< Fun, 1 >
 {
-  void eval(real* values, const real* x) const
+  Fun(): t(0) {}
+  void eval( real * values, const real * x ) const
   {
-    values[0] = sin(2.0 * DOLFIN_PI * x[0]) * cos(2.0 * DOLFIN_PI * x[1]) * cos(2.0 * DOLFIN_PI * x[2]) ; 
+    values[0] = exp(t) * sin(2.0 * DOLFIN_PI * x[0]) * cos(2.0 * DOLFIN_PI * x[1]) * cos(2.0 * DOLFIN_PI * x[2]) ; 
   }
+  double t;
 };
+
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
@@ -24,42 +26,52 @@ int main()
   dolfin_init();
 
   // Create mesh and a point in the mesh
-  UnitCube mesh(10, 10, 10);
+  //UnitCube mesh(50, 50, 50);
+  Mesh mesh("disc_with_transducers.bin");
+  real t = 0.0;
+  real Tfinal = 10.0;
+  real tstep = 0.1;
 
-  real x[3] = {0.444, 0.222, 0.133};
+  //real x[3] = {0.1,0.3,0.5};
+  real x[3] = {0.0, 0.0, -0.001035-0.00053};
   real f_values[1] = {0.0};
   real g_values[1] = {0.0};
 
-  // A user-defined function
-  Analytic<F> f(mesh);
+  Fun function;
 
   // Project to a discrete function
   Projection::BilinearForm a(mesh);
-  Projection::LinearForm L(mesh, f);
+  Function g(a.trial_space());
 
   // solve PDE
   Matrix A;
   Vector b;
   a.assemble(A, true);
-  L.assemble(b, true);
 
-  Function g(a.trial_space());
   KrylovSolver solver(bicgstab, bjacobi);
 
-  solver.solve(A, g.vector(), b);
-  g.sync();
-
   File( "function.pvd" ) << g;
-  
-  // Evaluate user-defined function f
-  f.eval(f_values, x);
-  message("f(x) = %g", f_values[0]);
 
-  // Evaluate discrete function g (projection of f)  
-  g.eval(g_values, x);
-  message("g(x) = %g", g_values[0]);
+  uint step = 0;
+  while (t < Tfinal)
+  {
+    function.t = t;
+    Analytic<Fun> f(mesh, function);
+    Projection::LinearForm L(mesh, f);
+    L.assemble(b, step == 0);
+    solver.solve(A, g.vector(), b);
+    g.sync();
 
+    // Evaluate user-defined function f
+    f.eval(f_values, x);
+    // Evaluate discrete function g (projection of f)  
+    g.eval(g_values, x);
+
+    // g.eval throws inf for no proc > 2 with bin mesh
+    message("time, f(x), g(x) = %g %g %g", t, f_values[0],  g_values[0]);
+    t +=tstep;
+    step += 1;
+  }
   dolfin_finalize();
-
   return 0;
 }
